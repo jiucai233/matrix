d5c3a3dd9d98e70380c3d0f35460298d868b3779
jiucai233 <jiucai@yonsei.ac.kr>
2024-05-13 16:52:11 +0900

    get matrix info

--- matrix.py
+++ matrix.py
@@ -1,3 +1,6 @@
 class Matrix:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,matrix):
+        self.matrix=matrix
+        self.row=(len(matrix))
+        self.col=(len(matrix[0]))
+        
